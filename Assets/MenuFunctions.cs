05c52d08f1aa54a1aad56ed418a91def09a998a3
Dana Woltz <dw952717@ohio.edu>
2021-07-01 23:33:04 -0400

    animationnnnnnnn

---
+++ Assets/MenuFunctions.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFunctions : MonoBehaviour
+{
+    public Animation cameraPan;
+
+    public void CameraPan()
+    {
+        cameraPan.Play();
+    }
+
+    private void Update()
+    {
+
+    }
+}
